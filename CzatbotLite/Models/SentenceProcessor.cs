cc37fabc8fb58b27569a03d85863efa366a33474
volodumurtyrko <volodumur.tyrko14@gmail.com>
2022-09-01 19:49:30 +0200

    Add project files.

---
+++ CzatbotLite/Models/SentenceProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CzatbotLite.Models;
+using NLipsum.Core;
+
+namespace CzatbotLite.Models
+{
+    internal class SentenceProcessor
+    {
+
+        public static string FindTheLongestWord(string sentence)
+        {
+            string[] strings = sentence.Split(' '); 
+            string word = "";
+            int i = 0;
+
+            while (i < strings.Length)
+                if (strings[i].Length > word.Length)
+                {
+                    word = strings[i];
+                    i++;
+                }
+                else
+                {
+                    i++;
+                    continue;
+                }
+
+            return word;
+        }
+
+    }
+}
+
---
+++ CzatbotLite/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CzatbotLite.Models;
+
+namespace CzatbotLite
+{
+    internal class Program
+    {
+
+
+
+          static void Main(string[]args)
+        {
+            Console.WriteLine("Enter your name.");           
+            string name =  Console.ReadLine(); //Volodymyr
+            Console.WriteLine("Helow " + name + " How are you?");
+            string answer = Console.ReadLine(); // Very good
+            Console.WriteLine("What does mean " + answer);
+            string sentence = Console.ReadLine(); // Very good is good deeds
+
+            string word = SentenceProcessor.FindTheLongestWord(sentence);
+            Console.WriteLine(word);
+            Console.ReadKey();
+        }
+    }
+}
